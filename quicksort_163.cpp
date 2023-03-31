d39cf276038081730cb7bafea68ae0b8a0fa757f
DimasCW <cdimas292@gmail.com>
2023-03-31 10:57:33 +0700

    Add project files.

---
+++ quicksort_163.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//array of integrers to hold values
+int arr[20];
+int cmp_count = 0;// number of comparison
+int mov_count = 0;// number of movement
+int n;
+void input() {
+	while (true) {
+		cout << "masukkan oanjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n maksimum panjang element array : "
+	}
+}
