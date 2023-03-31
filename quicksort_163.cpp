32f468b2eb899fb674cb2b6cb6cfcb2558abc12a
DimasCW <cdimas292@gmail.com>
2023-03-31 11:01:23 +0700

    2

--- quicksort_163.cpp
+++ quicksort_163.cpp
@@ -14,6 +14,16 @@ void input() {
 		if (n <= 20)
 			break;
 		else
-			cout << "\n maksimum panjang element array : "
+			cout << "\n maksimum panjang arrary adalah 20" << endl;
 	} 
+
+	cout << "\n--------------------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n--------------------------" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
+
